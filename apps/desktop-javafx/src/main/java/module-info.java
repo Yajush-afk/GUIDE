0514fb9098c41daf3a8aa2f9a49392ac468fcbaf
godly-AK <adarsh.krishna5447@gmail.com>
2026-06-23 23:33:58 +0530

    Revert "added boilerplate code for app"
    This reverts commit 3e6398e195297f1de8774710033d49c3e72ccc3c.

--- apps/desktop-javafx/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.guide.desktop {
-    // The app uses JavaFX controls and layouts built directly in Java code.
-    requires javafx.controls;
-
-    // Exports the application package so the JavaFX launcher can start GuideApp.
-    exports com.guide.desktop;
-}
